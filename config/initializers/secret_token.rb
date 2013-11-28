1200cbea5287f8acd8cb76dc18ce0f6de5a9c0c3
bbeaird <bbeaird@gmail.com>
2013-11-27 19:21:59 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrphanCrazyEyes::Application.config.secret_token = 'faa19c48f95e5f9559a51885814e0951aaf4e897cdd0b540036a699cf0d9ba4cee2730a0943d884d1b8e6a1d0260ae0b63a572b1e8ba73f752b948df1bced613'
