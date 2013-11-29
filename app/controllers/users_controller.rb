52d60844732a0c4f1415bae9e92d8f1e74bf6b70
vanessa <vipyne@gmail.com>
2013-11-28 16:58:56 -0800

    f apis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@ class UsersController <ApplicationController
 # order: index show new create edit update destroy
 
   def index
+
   end
 
   def create
     p "* " * 50
-    p auth_hash.credentials.token
-    @instagram = Instagram.user_recent_media("16676245", {:count => 1})
+    # p auth_hash.credentials.token
+    # @cred = auth_hash.credentials.token
+    # @instagram = Instagram.user_recent_media("16676245", {:count => 1})
 
     debugger
   end
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -4,6 +4,6 @@
 
     config.client_id = ENV['INSTAGRAM_CLIENT_ID']
 
-    config.access_token = "access_token goes here"
+    config.access_token = "16676245.42e865a.f9b08f777df24baf96bd3718c72767a6"
 
     end
