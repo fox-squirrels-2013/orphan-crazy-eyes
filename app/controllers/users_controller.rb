cadafba261990042c9d15a5b14c06fba998a6760
vanessa <vipyne@gmail.com>
2013-11-28 16:47:33 -0800

    settin up user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,13 @@ class UsersController <ApplicationController
 # order: index show new create edit update destroy
 
   def index
-    # @instagram = Instagram.user_recent_media("user_id_here", {:count => 1})
   end
 
   def create
+    p "* " * 50
+    p auth_hash.credentials.token
+    @instagram = Instagram.user_recent_media("16676245", {:count => 1})
+
     debugger
   end
 
