cf2b24a66d2aa728d753c8ff582dcb84d6cff68d
Ed Shadi <edshadi@gmail.com>
2013-11-30 08:16:19 -0800

    remove unused new action and view use find_or_create_by_hash remove redirect_to_instagram, it's already defined in auth lib.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,13 @@
 class UsersController <ApplicationController
   before_filter :redirect_to_instagram, :only => [:create]
-# order: index show new create edit update destroy
-
-  def new
-
-  end
 
   def create
-    user = User.new :uid => auth_hash.uid, :access_token => auth_hash.credentials.token, :nickname => auth_hash.info.nickname
-    if user.save
-      session[:user_id] = user.id
-      session[:access_token] = auth_hash.credentials.token
+    user = User.find_or_create_by_auth auth_hash
+    if user.present?
+      login user
       redirect_to instagram_path(current_user)
     else
-      redirect_to root_path
-      # handle errors
+      redirect_to root_path, :error => "Something Went Wrong"
     end
   end
-
-  private
-
-  def redirect_to_instagram
-    redirect_to instagram_path(current_user) if current_user
-  end
-
 end
