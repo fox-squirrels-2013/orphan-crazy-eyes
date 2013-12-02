6ab2cb761ca39062884e22b8617037d65060a3bd
vanessa <vipyne@gmail.com>
2013-12-01 23:29:44 -0800

    basic lob module setup

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,10 @@
 class Subscription < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :address_line1,
-  :address_line2, :city, :state, :country, :zip
+  :address_line2, :city, :state, :country, :zip, :user_id
   belongs_to :user
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
 
 end
