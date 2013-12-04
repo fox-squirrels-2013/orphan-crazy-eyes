4bfc57541e373461fccf636377eb90683d1e35a2
danbender <=>
2013-12-04 11:44:14 -0800

    removed test link test and added pending route test

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe "login via instagram" do
-  it "clicking on login routes to instagram login page" do
+
+  xit "clicking on login routes to instagram login page" do
     visit root_path
     page.has_content?("#collect_a_matron")
     # login_with_oauth
@@ -9,10 +10,4 @@ describe "login via instagram" do
     page.has_content?("Forgot password?")
   end
 
-  it "clicking on login routes to instagram login page" do
-    visit root_path
-    page.has_content?("#collect_a_matron")
-    click_link("AOL you know for kids!")
-    page.has_content?("WELCOME, SIGN IN")
-  end
 end
