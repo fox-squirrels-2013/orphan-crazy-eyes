8d2c60ba0998228b26be142fda18e0a25d4d4d26
vanessa <vipyne@gmail.com>
2013-12-23 16:37:24 -0500

    heart replaced and vote in place

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 
 $('button.vote-button').on('click', function(event) {
   event.preventDefault()
-    debugger
   var $thisButton = $(this)
   var $heart = $(this).children()
   var id = $(this).siblings().attr('id')
@@ -29,9 +28,7 @@ $('button[disabled=false]').attr('disabled', false)
 $('button.vote-button[disabled=true]').children().css('background', 'rgba(195,195,195,1)')
 
 /////////////////////////////
-// ToDo: Since this hides 2 and un-hides 2 each time,
-// can this be refactored to use button and then style
-// button to blend in with the navbar
+// toggle not presently a feature
 
 $('li.all-pics').on('click', function(event) {
   event.preventDefault()
