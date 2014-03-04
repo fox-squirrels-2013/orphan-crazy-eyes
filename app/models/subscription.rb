53b49c7faa883c16bba72e46c2aec0c418173e95
vanessa <vipyne@gmail.com>
2014-03-04 11:31:18 -0500

    added email to view and validations to model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,8 +1,8 @@
 class Subscription < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :address_line1,
+  attr_accessible :first_name, :last_name, :email, :address_line1,
   :address_line2, :city, :state, :country, :zip, :user_id
   belongs_to :user
-  validates :first_name, :last_name, :address_line1, :city, :state, :country, :zip, :user_id, :presence => true
+  validates :first_name, :last_name, :email, :address_line1, :city, :state, :country, :zip, :user_id, :presence => true
 
   def full_name
     self.first_name + " " + self.last_name
